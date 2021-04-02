f390a6c17a8ba812aeab4cd0f1e0515344709fbb
Nathan Miller <natem31190@gmail.com>
2021-04-01 22:01:56 -0400

    commit tree 2f2b520cbeb557f5631be6f4a87d0c242719c8e5

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20210402015835) do
+    create_table "users", force: :cascade do |t|
+        t.string "email"
+        t.string "username"
+        t.string "password_digest"
+        t.datetime "created_at"
+        t.datetime "updated_at"
+      end
 
 end
